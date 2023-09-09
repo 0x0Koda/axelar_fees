8e73840046dc8edfd0da4382f476a5bae6b31835
Koda <12773720+0x0Koda@users.noreply.github.com>
2023-09-09 10:45:20 +1000

    initial commit

---
+++ index.ts
@@ -0,0 +1,47 @@
+import {
+  AxelarQueryAPI,
+  AxelarQueryAPIConfig,
+  Environment as AxelarEnvironment,
+} from "@axelar-network/axelarjs-sdk";
+
+(async function () {
+  const sdk = new AxelarQueryAPI({
+    environment: AxelarEnvironment.MAINNET,
+  } as AxelarQueryAPIConfig);
+
+  let gasEstimateResponse1 = await sdk.estimateGasFee(
+    "osmosis",
+    "Avalanche",
+    "aUSDC",
+    220000,
+    1,
+    undefined,
+    {
+      showDetailedFees: true,
+      transferAmount: 220000,
+      destinationContractAddress:
+        "0xce16F69375520ab01377ce7B88f5BA8C48F8D666" as string,
+      sourceContractAddress: "" as string,
+      tokenSymbol: "aUSDC",
+    }
+  );
+  console.log(gasEstimateResponse1);
+
+  let gasEstimateResponse2 = await sdk.estimateGasFee(
+    "osmosis",
+    "Avalanche",
+    "aUSDC",
+    220000,
+    1,
+    undefined,
+    {
+      showDetailedFees: true,
+      transferAmount: 220000,
+      destinationContractAddress:
+        "0xce16F69375520ab01377ce7B88f5BA8C48F8D666" as string,
+      sourceContractAddress: "" as string,
+      tokenSymbol: "aUSDC",
+    }
+  );
+  console.log(gasEstimateResponse2);
+})();
